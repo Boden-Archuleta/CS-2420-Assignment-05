cbd36a582b8f316bfa87264347534ee2b3ea989e
Boden-Archuleta <boden.archuleta@gmail.com>
2017-10-25 19:57:24 -0600

    implemented most methods

--- src/assignment05/TagTree.java
+++ src/assignment05/TagTree.java
@@ -5,18 +5,20 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
+import org.omg.CORBA.Current;
+
 public class TagTree {
 	
-	private Node root;
+	public Node root;
 	
 	private static class Node{
 		String data;
 		String id;
-		Set<Node> children;
+		ArrayList<Node> children;
 		Node(String i, String d){
 			id = i;
 			data = d;
-			children = new HashSet<Node>();
+			children = new ArrayList<Node>();
 		}
 	}
 
@@ -28,13 +30,12 @@ public class TagTree {
 	private void scan(Node current, Scanner ss){
 		while (ss.hasNext()){
 			String next = ss.next();
-			
-			while(!isEndTag(next)){
+			if (isEndTag(next)){
+				return;
+			}
+			else {
 				Node child = parseNode(ss, next);
-				next = ss.next();
-				if (!isEndTag(next)){
-					scan(child, ss);
-				}
+				scan(child, ss);
 				current.children.add(child);
 			}
 		}
@@ -57,42 +58,132 @@ public class TagTree {
 	}
 	
 	public int getHeight(){
+		return getHeight(root, 0);
+	}
+	
+	private int getHeight(Node current, int level){
+		if (current.children.size() == 0)
+			return level;
+		level++;
 		
-		return 0;
+		for (Node n : current.children){
+			level = getHeight(n, level);
+		}
+		
+		return level;
 	}
 	
 	public int getMaxDegree(){
+		return getMaxDegree(root, 0);
+	}
+	
+	private int getMaxDegree(Node current, int degree){
+		if (current.children.size() == 0)
+			return degree;
 		
-		return 0;
+		for (Node n : current.children){
+			degree++;
+			Math.max(degree, getMaxDegree(n, 0));
+		}
+		
+		return degree;
 	}
 	
 	public String outputPrefix(){
+		return outputPrefix(root, "");
+	}
+	
+	private String outputPrefix(Node current, String output){
+		output = output.concat(current.data);
 		
-		return null;
+		if (current.children.size() == 0){
+			return output;
+		}
+		
+		for (Node n : current.children){
+			output = output.concat(" ");
+			output = outputPrefix(n, output);
+		}
+		
+		return output;
 	}
 	
 	public String outputPostfix(){
+		return outputPostfix(root, "");
+	}
+	
+	private String outputPostfix(Node current, String output){
+		if (current.children.size() == 0){
+			return output.concat(current.data);
+		}
 		
-		return null;
+		for (Node n : current.children){
+			output = outputPostfix(n, output);
+			output = output.concat(" ");
+		}
+		
+		output = output.concat(current.data);
+		
+		return output;
 	}
 	
 	public boolean isBinaryTree(){
+		if (this.getMaxDegree(root, 0) > 2){
+			return false;
+		}
 		
 		return true;
 	}
 	
 	public boolean isTwoTree(){
+		return isTwoTree(root);
+	}
+	
+	private boolean isTwoTree(Node current){
+		if (current.children.size() != 2 && current.children.size() != 0){
+			return false;
+		}
+		
+		for (Node n : current.children){
+			isTwoTree(n);
+		}
 		
 		return true;
 	}
 	
 	public boolean isFullBinaryTree(){
+		int levels = this.getHeight();
 		
-		return true;
+		int numNodes = this.nodeCount(root, 0);
+		if ((Math.pow(2, levels) - 1 == numNodes)){
+			return true;
+		}
+		
+		return false;
+	}
+	
+	private int nodeCount(Node current, int count){
+		count++;
+		if (current.children.size() == 0){
+			return count;
+		}
+		
+		for (Node n : current.children){
+			count = nodeCount(n, count);
+		}
+		
+		return count;
 	}
 	
 	public int findDepth(String word){
-		
+		return findDepth(word, root, 0, -1);
+	}
+	
+	private int findDepth(String word, Node current, int level, int val){
 		return 0;
 	}
+	
+	
+	
+	
 }
--- src/assignment05/testTagTree.java
+++ src/assignment05/testTagTree.java
@@ -3,6 +3,8 @@ package assignment05;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 import org.junit.After;
@@ -13,6 +15,7 @@ public class testTagTree {
 
 	@Before
 	public void setUp() throws Exception {
+		
 	}
 
 	@After
@@ -20,9 +23,101 @@ public class testTagTree {
 	}
 
 	@Test
-	public void testParse() {
-		Scanner ss = new Scanner("<a blue> <b green> </b> <c blue> </c> <d yellow> </d> </a>");
+	public void testParse() throws IOException {
+		File f = new File("src/assignment05/input.txt");
+		Scanner ss = new Scanner(f);
 		TagTree tree = new TagTree(ss);
+		ss.close();
 	}
 
+	@Test
+	public void testMaxHeight() throws FileNotFoundException{
+		File f = new File("src/assignment05/input.txt");
+		Scanner ss = new Scanner(f);
+		TagTree tree = new TagTree(ss);
+		
+		assertEquals(2, tree.getHeight(), 0);
+		
+		ss.close();
+	}
+	
+	@Test
+	public void testMaxDegree() throws FileNotFoundException{
+		File f = new File("src/assignment05/input.txt");
+		Scanner ss = new Scanner(f);
+		TagTree tree = new TagTree(ss);
+		
+		assertEquals(3, tree.getMaxDegree());
+		
+		ss.close();
+	}
+	
+	@Test
+	public void testPreOrder() throws FileNotFoundException{
+		File f = new File("src/assignment05/input.txt");
+		Scanner ss = new Scanner(f);
+		TagTree tree = new TagTree(ss);
+		
+		assertEquals("blue red green yellow orange", tree.outputPrefix());
+		
+		ss.close();
+	}
+	
+	
+	@Test 
+	public void testPostOrder() throws FileNotFoundException{
+		File f = new File("src/assignment05/input.txt");
+		Scanner ss = new Scanner(f);
+		TagTree tree = new TagTree(ss);
+		
+		assertEquals("red yellow green orange blue", tree.outputPostfix());
+		
+		ss.close();
+	}
+	
+	
+	@Test
+	public void testBinaryTree() throws FileNotFoundException{
+		File f = new File("src/assignment05/input.txt");
+		Scanner ss = new Scanner(f);
+		TagTree tree = new TagTree(ss);
+		
+		assertEquals(false, tree.isBinaryTree());
+		
+		ss.close();
+	}
+	
+	@Test
+	public void twoTreeTest() throws FileNotFoundException{
+		File f = new File("src/assignment05/inputTwo.txt");
+		Scanner ss = new Scanner(f);
+		TagTree tree = new TagTree(ss);
+		
+		assertEquals(true, tree.isTwoTree());
+		
+		ss.close();
+	}
+	
+	@Test
+	public void fullTreeTest() throws FileNotFoundException{
+		File f = new File("src/assignment05/inputTwo.txt");
+		Scanner ss = new Scanner(f);
+		TagTree tree = new TagTree(ss);
+		
+		assertEquals(true, tree.isFullBinaryTree());
+		
+		ss.close();
+	}
+	
+	@Test
+	public void testFindLevel() throws FileNotFoundException{
+		File f = new File("src/assignment05/input.txt");
+		Scanner ss = new Scanner(f);
+		TagTree tree = new TagTree(ss);
+		
+		assertEquals(1, tree.findDepth("red"));
+		
+		ss.close();
+	}
+	
 }
